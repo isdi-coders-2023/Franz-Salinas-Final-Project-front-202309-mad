b1f0ec81d68919ce667b37c9b70d3140d555868c
Franz <fernandofranquiz@gmail.com>
2023-12-07 11:59:22 +0100

    Add commplete funtionality of login and register

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,3 +1,37 @@
+import { SyntheticEvent, useState } from 'react';
+import { useUsers } from '../../hooks/user.hooks';
+
+import { LoginUser } from '../../models/users';
+
 export const Login = () => {
-  return <></>;
+  const [isLogged, setLogin] = useState(false);
+  const { login } = useUsers();
+
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    const formElement = event.target as HTMLFormElement;
+    const formData = new FormData(formElement);
+    const loginUser: LoginUser = {
+      email: formData.get('email')?.toString() as string,
+      password: formData.get('password')?.toString() as string,
+    };
+    login(loginUser);
+    setLogin(true);
+  };
+
+  return (
+    <>
+      {!isLogged && (
+        <form onSubmit={handleSubmit} className="login-form">
+          <label htmlFor="">Email</label>
+          <input type="text" name="email" required />
+          <label htmlFor="">Password</label>
+          <input type="text" name="password" required />
+          <button type="submit"> Login </button>
+        </form>
+      )}
+
+      {isLogged && <p>Estas logeado papi</p>}
+    </>
+  );
 };
--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -1,24 +1,46 @@
+import { SyntheticEvent, useState } from 'react';
+import { useUsers } from '../../hooks/user.hooks';
+
 export const Register = () => {
+  const [hasRegister, setRegister] = useState(false);
+  const { register } = useUsers();
+
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    const formElement = event.target as HTMLFormElement;
+    const formData = new FormData(formElement);
+    register(formData);
+    setRegister(true);
+
+    return;
+  };
+
   return (
     <>
-      <form>
-        <label htmlFor="">Email</label>
-        <input type="text" name="email" />
-        <label htmlFor="">Name</label>
-        <input type="text" name="name" placeholder="name" />
-        <label htmlFor="">Surname</label>
-        <input type="text" name="surname" placeholder="surname" />
-        <label htmlFor="">Age</label>
-        <input type="text" name="age" placeholder="age" />
-        <label htmlFor="">Password</label>
-        <input type="text" name="password" />
-        <label htmlFor="">Avatar</label>
-        <input type="text" name="avatar" />
-        <label htmlFor="">Style of Play</label>
-        <input type="text" name="style" />
-        <label htmlFor="">Team Name</label>
-        <input type="text" name="Password" />
-      </form>
+      <h2>Register</h2>
+
+      {!hasRegister && (
+        <form onSubmit={handleSubmit} className="register-form">
+          <label htmlFor="">Email</label>
+          <input type="text" name="email" required />
+          <label htmlFor="">Name</label>
+          <input type="text" name="name" />
+          <label htmlFor="">Surname</label>
+          <input type="text" name="surname" />
+          <label htmlFor="">Age</label>
+          <input type="text" name="age" />
+          <label htmlFor="">Password</label>
+          <input type="text" name="password" required />
+          <label htmlFor="">Avatar</label>
+          <input type="file" name="avatar" required />
+          <label htmlFor="">Style of Play</label>
+          <input type="text" name="style" />
+          <label htmlFor="">Team Name</label>
+          <input type="text" name="Password" />
+          <button type="submit">Register</button>
+          <button type="button">Cancel</button>
+        </form>
+      )}
     </>
   );
 };
