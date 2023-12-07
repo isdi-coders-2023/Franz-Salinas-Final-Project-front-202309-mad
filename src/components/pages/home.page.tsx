e5de83b67e944237ea5071927ad5e93df11daa89
Franz <fernandofranquiz@gmail.com>
2023-12-07 21:30:33 +0100

    Add better css for home.page

--- src/components/pages/home.page.tsx
+++ src/components/pages/home.page.tsx
@@ -3,31 +3,34 @@ import './home.page.scss';
 export const HomePage = () => {
   return (
     <>
-      <div className="main-top-home-page-container">
-        <div className="welcome-message-container">
-          <p className="main-message">Welcome to Dream Team FC</p>
-          <p className="lower-message">Build the team of your dreams</p>
-          <p className="lower-message">with the best players of the world</p>
+      <div className="main-container">
+        <div className="main-top-home-page-container">
+          <div className="welcome-message-container">
+            <p className="main-message">Welcome to Dream Team FC</p>
+            <p className="lower-message">Build the team of your dreams</p>
+            <p className="lower-message">with the best players of the world</p>
+          </div>
+          <div className="top-image-container">
+            <img
+              src="https://res.cloudinary.com/dnlkezvzv/image/upload/h_550/v1701952660/proyecto%20final/uokyyukezq1jm0msaavr.jpg"
+              alt="debruyne"
+            />
+          </div>
         </div>
-        <div className="top-image-container">
-          <img
-            src="https://res.cloudinary.com/dnlkezvzv/image/upload/h_550/v1701952660/proyecto%20final/uokyyukezq1jm0msaavr.jpg"
-            alt="debruyne"
-          />
-        </div>
-      </div>
-      <div className="main-botton-home-page-container">
-        <div className="image-formation-container">
-          <img
-            src="https://res.cloudinary.com/dnlkezvzv/image/upload/h_450/v1701972255/proyecto%20final/wbzvsrmccucswwsiztaw.jpg"
-            alt="formation"
-          />
-        </div>
-        <div className="card-footballer-container">
-          <img
-            src="https://res.cloudinary.com/dnlkezvzv/image/upload/h_450/v1701973640/proyecto%20final/gm4wrmhxfqvwtzmjmhrb.webp"
-            alt="card-footballer"
-          />
+        <div className="main-botton-home-page-container">
+          <div className="image-formation-container">
+            <img
+              className="football-field"
+              src="https://res.cloudinary.com/dnlkezvzv/image/upload/h_450/v1701972255/proyecto%20final/wbzvsrmccucswwsiztaw.jpg"
+              alt="formation"
+            />
+          </div>
+          <div className="card-footballer-container">
+            <img
+              src="https://res.cloudinary.com/dnlkezvzv/image/upload/h_450/v1701973640/proyecto%20final/gm4wrmhxfqvwtzmjmhrb.webp"
+              alt="card-footballer"
+            />
+          </div>
         </div>
       </div>
     </>
