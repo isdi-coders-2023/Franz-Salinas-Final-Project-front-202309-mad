2592b516c9a8200e8439b815a0765e15e787152e
Franz <fernandofranquiz@gmail.com>
2023-12-11 19:10:03 +0100

    Add display grid for list component

--- src/components/app.routes/app.routes.tsx
+++ src/components/app.routes/app.routes.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../pages/login.page';
 import { RegisterPage } from '../pages/register.page';
 import { HomePage } from '../pages/home.page';
-import { MyPlayers } from '../my-players/my.players';
+import { MyPlayers } from '../pages/my.players';
 
 export const AppRoutes = () => {
   return (
