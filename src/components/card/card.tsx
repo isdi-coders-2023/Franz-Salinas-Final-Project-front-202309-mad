014327bf749b01dcb64f0bda0141253e66023251
Franz <fernandofranquiz@gmail.com>
2023-12-11 21:13:13 +0100

    Add partial media query for card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -14,55 +14,59 @@ export const Card = ({ info }: Props) => {
 
   return (
     <>
-      <div className="fut-player-card">
-        <div className="player-card-top">
-          <div className="player-master-info">
-            <div className="player-rating">
-              <span>{info.overall}</span>
-            </div>
-            <div className="player-position">
-              <span>{info.position}</span>
-            </div>
-          </div>
-          <div className="player-picture">
-            <img src={footballerImage} alt="Messi" draggable="false" />
-          </div>
-        </div>
-
-        <div className="player-card-bottom">
-          <div className="player-info">
-            <div className="player-name">
-              <span>{info.name}</span>
-            </div>
-
-            <div className="player-features">
-              <div className="player-features-col">
-                <span>
-                  <div className="player-feature-value">{info.pace}</div>
-                  <div className="player-feature-title">PAC</div>
-                </span>
-                <span>
-                  <div className="player-feature-value">{info.shoot}</div>
-                  <div className="player-feature-title">SHO</div>
-                </span>
-                <span>
-                  <div className="player-feature-value">{info.passing}</div>
-                  <div className="player-feature-title">PAS</div>
-                </span>
+      <div className=".fut-player-cards-container">
+        <div className="fut-player-card">
+          <div className="player-card-top">
+            <div className="player-master-info">
+              <div className="player-rating">
+                <span></span>
               </div>
-              <div className="player-features-col">
-                <span>
-                  <div className="player-feature-value">{info.drible}</div>
-                  <div className="player-feature-title">DRI</div>
-                </span>
-                <span>
-                  <div className="player-feature-value">{info.defense}</div>
-                  <div className="player-feature-title">DEF</div>
-                </span>
-                <span>
-                  <div className="player-feature-value">{info.physicality}</div>
-                  <div className="player-feature-title">PHY</div>
-                </span>
+              <div className="player-position">
+                <span>{info.position}</span>
+              </div>
+            </div>
+            <div className="player-picture">
+              <img src={footballerImage} alt="Messi" draggable="false" />
+            </div>
+          </div>
+
+          <div className="player-card-bottom">
+            <div className="player-info">
+              <div className="player-name">
+                <span>{info.overall}</span>
+              </div>
+
+              <div className="player-features">
+                <div className="player-features-col">
+                  <span>
+                    <div className="player-feature-value">{info.pace}</div>
+                    <div className="player-feature-title">PAC</div>
+                  </span>
+                  <span>
+                    <div className="player-feature-value">{info.shoot}</div>
+                    <div className="player-feature-title">SHO</div>
+                  </span>
+                  <span>
+                    <div className="player-feature-value">{info.passing}</div>
+                    <div className="player-feature-title">PAS</div>
+                  </span>
+                </div>
+                <div className="player-features-col">
+                  <span>
+                    <div className="player-feature-value">{info.drible}</div>
+                    <div className="player-feature-title">DRI</div>
+                  </span>
+                  <span>
+                    <div className="player-feature-value">{info.defense}</div>
+                    <div className="player-feature-title">DEF</div>
+                  </span>
+                  <span>
+                    <div className="player-feature-value">
+                      {info.physicality}
+                    </div>
+                    <div className="player-feature-title">PHY</div>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
