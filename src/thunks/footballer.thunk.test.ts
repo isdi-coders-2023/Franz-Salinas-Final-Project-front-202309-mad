16aa3385e1febd7e44eff89554dc3bf40ea8a8e4
Franz <fernandofranquiz@gmail.com>
2023-12-12 15:59:13 +0100

    Add footballer.thunks test

---
+++ src/thunks/footballer.thunk.test.ts
@@ -0,0 +1,20 @@
+import { store } from '../store/store';
+import { FootballerRepo } from '../services/footballer.repo';
+
+import { loadFootballersThunk } from './footballer.thunk';
+
+describe('Given loadFootballersThunks ...', () => {
+  describe('When we use them', () => {
+    const mockRepo = {
+      repo: {
+        getFootballers: jest.fn().mockReturnValue([]),
+      } as unknown as FootballerRepo,
+    };
+
+    test('Then the getFootballers should have been called...', async () => {
+      const data = { ...mockRepo } as { repo: FootballerRepo };
+      await store.dispatch(loadFootballersThunk(data.repo));
+      expect(data.repo.getFootballers).toHaveBeenCalled();
+    });
+  });
+});
