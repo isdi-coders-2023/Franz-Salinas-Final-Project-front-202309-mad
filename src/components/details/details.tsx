e11c3f1e325df6604683678a11d70458cddfac12
Franz <fernandofranquiz@gmail.com>
2023-12-19 13:48:28 +0100

    Partial scss of details page

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -14,16 +14,58 @@ export const Details = () => {
   return (
     <>
       <div className="details-page-container" data-testid="details-container">
-        <h2>{currentFootballer?.name}</h2>
-
         <div className="image-details-container">
           <img src={detailsFootballerImage!} alt="footballer-image-details" />
         </div>
+
         <div className="data-container">
-          {' '}
-          <p className="footballer-name"> {currentFootballer?.name}</p>
-          <p className=""> {currentFootballer?.nationality}</p>
-          <p className=""> {currentFootballer?.preferFoot}</p>
+          <div className="data-player-name">
+            {' '}
+            <p> {currentFootballer?.name}</p>
+          </div>
+
+          <div className="data-group">
+            <p className="custom-details-writting">
+              Shoot{' '}
+              <span className="data-small-container">
+                {currentFootballer?.shoot}
+              </span>
+            </p>
+            <p className="custom-details-writting">
+              Passing{' '}
+              <span className="data-small-container">
+                {currentFootballer?.passing}
+              </span>
+            </p>
+          </div>
+          <div className="data-group">
+            <p className="custom-details-writting">
+              Pace{' '}
+              <span className="data-small-container">
+                {currentFootballer?.pace}
+              </span>
+            </p>
+            <p className="custom-details-writting">
+              Drible{' '}
+              <span className="data-small-container">
+                {currentFootballer?.drible}
+              </span>
+            </p>
+          </div>
+          <div className="data-group">
+            <p className="custom-details-writting">
+              Defense{' '}
+              <span className="data-small-container">
+                {currentFootballer?.defense}
+              </span>
+            </p>
+            <p className="custom-details-writting">
+              Phisicality{' '}
+              <span className="data-small-container">
+                {currentFootballer?.physicality}
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </>
